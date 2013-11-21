938f01bcce801167e9e7de2afaeedbf8a77576cd
Nathan Laan <nathanlaan@gmail.com>
2013-11-21 15:39:47 -0700

    Working on UI.

--- NetUtil.App/AppForm.Designer.cs
+++ NetUtil.App/AppForm.Designer.cs
@@ -43,10 +43,14 @@
             this.tsc = new System.Windows.Forms.ToolStripContainer();
             this.tsMain = new System.Windows.Forms.ToolStrip();
             this.tsbStartStop = new System.Windows.Forms.ToolStripButton();
+            this.splitContainerMainV = new System.Windows.Forms.SplitContainer();
             this.mnu.SuspendLayout();
+            this.tsc.ContentPanel.SuspendLayout();
             this.tsc.TopToolStripPanel.SuspendLayout();
             this.tsc.SuspendLayout();
             this.tsMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMainV)).BeginInit();
+            this.splitContainerMainV.SuspendLayout();
             this.SuspendLayout();
             // 
             // mnu
@@ -56,7 +60,8 @@
             this.mnuHelp});
             this.mnu.Location = new System.Drawing.Point(0, 0);
             this.mnu.Name = "mnu";
-            this.mnu.Size = new System.Drawing.Size(622, 28);
+            this.mnu.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.mnu.Size = new System.Drawing.Size(466, 24);
             this.mnu.TabIndex = 0;
             this.mnu.Text = "menuStrip1";
             // 
@@ -65,13 +70,13 @@
             this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuFileExit});
             this.mnuFile.Name = "mnuFile";
-            this.mnuFile.Size = new System.Drawing.Size(44, 24);
+            this.mnuFile.Size = new System.Drawing.Size(35, 20);
             this.mnuFile.Text = "&File";
             // 
             // mnuFileExit
             // 
             this.mnuFileExit.Name = "mnuFileExit";
-            this.mnuFileExit.Size = new System.Drawing.Size(152, 24);
+            this.mnuFileExit.Size = new System.Drawing.Size(92, 22);
             this.mnuFileExit.Text = "E&xit";
             this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
             // 
@@ -80,13 +85,13 @@
             this.mnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuHelpAbout});
             this.mnuHelp.Name = "mnuHelp";
-            this.mnuHelp.Size = new System.Drawing.Size(53, 24);
+            this.mnuHelp.Size = new System.Drawing.Size(40, 20);
             this.mnuHelp.Text = "&Help";
             // 
             // mnuHelpAbout
             // 
             this.mnuHelpAbout.Name = "mnuHelpAbout";
-            this.mnuHelpAbout.Size = new System.Drawing.Size(152, 24);
+            this.mnuHelpAbout.Size = new System.Drawing.Size(103, 22);
             this.mnuHelpAbout.Text = "&About";
             // 
             // BottomToolStripPanel
@@ -127,9 +132,10 @@
             // 
             // sts
             // 
-            this.sts.Location = new System.Drawing.Point(0, 411);
+            this.sts.Location = new System.Drawing.Point(0, 330);
             this.sts.Name = "sts";
-            this.sts.Size = new System.Drawing.Size(622, 22);
+            this.sts.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.sts.Size = new System.Drawing.Size(466, 22);
             this.sts.TabIndex = 1;
             this.sts.Text = "statusStrip1";
             // 
@@ -138,11 +144,14 @@
             // 
             // tsc.ContentPanel
             // 
-            this.tsc.ContentPanel.Size = new System.Drawing.Size(622, 356);
+            this.tsc.ContentPanel.Controls.Add(this.splitContainerMainV);
+            this.tsc.ContentPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tsc.ContentPanel.Size = new System.Drawing.Size(466, 281);
             this.tsc.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tsc.Location = new System.Drawing.Point(0, 28);
+            this.tsc.Location = new System.Drawing.Point(0, 24);
+            this.tsc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tsc.Name = "tsc";
-            this.tsc.Size = new System.Drawing.Size(622, 383);
+            this.tsc.Size = new System.Drawing.Size(466, 306);
             this.tsc.TabIndex = 3;
             this.tsc.Text = "toolStripContainer1";
             // 
@@ -157,7 +166,7 @@
             this.tsbStartStop});
             this.tsMain.Location = new System.Drawing.Point(3, 0);
             this.tsMain.Name = "tsMain";
-            this.tsMain.Size = new System.Drawing.Size(103, 27);
+            this.tsMain.Size = new System.Drawing.Size(61, 25);
             this.tsMain.TabIndex = 0;
             // 
             // tsbStartStop
@@ -166,30 +175,43 @@
             this.tsbStartStop.Image = ((System.Drawing.Image)(resources.GetObject("tsbStartStop.Image")));
             this.tsbStartStop.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbStartStop.Name = "tsbStartStop";
-            this.tsbStartStop.Size = new System.Drawing.Size(60, 24);
+            this.tsbStartStop.Size = new System.Drawing.Size(51, 22);
             this.tsbStartStop.Text = "Start";
             this.tsbStartStop.Click += new System.EventHandler(this.tsbStartStop_Click);
             // 
+            // splitContainerMainV
+            // 
+            this.splitContainerMainV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMainV.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerMainV.Name = "splitContainerMainV";
+            this.splitContainerMainV.Size = new System.Drawing.Size(466, 281);
+            this.splitContainerMainV.SplitterDistance = 155;
+            this.splitContainerMainV.TabIndex = 0;
+            // 
             // AppForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 433);
+            this.ClientSize = new System.Drawing.Size(466, 352);
             this.Controls.Add(this.tsc);
             this.Controls.Add(this.sts);
             this.Controls.Add(this.mnu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.mnu;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AppForm";
             this.Text = "NetUtil";
             this.mnu.ResumeLayout(false);
             this.mnu.PerformLayout();
+            this.tsc.ContentPanel.ResumeLayout(false);
             this.tsc.TopToolStripPanel.ResumeLayout(false);
             this.tsc.TopToolStripPanel.PerformLayout();
             this.tsc.ResumeLayout(false);
             this.tsc.PerformLayout();
             this.tsMain.ResumeLayout(false);
             this.tsMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMainV)).EndInit();
+            this.splitContainerMainV.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,6 +233,7 @@
         private System.Windows.Forms.ToolStripContainer tsc;
         private System.Windows.Forms.ToolStrip tsMain;
         private System.Windows.Forms.ToolStripButton tsbStartStop;
+        private System.Windows.Forms.SplitContainer splitContainerMainV;
     }
 }
 
